09e46d2f17e757a082d7016edba8795935294a74
Abdul Ateeb <abdulateeb5932@gmail.com>
2026-07-11 14:04:38 +0530

    Rename the page title to Articles
    Browser tab now shows Articles.

--- apps/web/app/(app)/documents/page.tsx
+++ apps/web/app/(app)/documents/page.tsx
@@ -1,6 +1,6 @@
 import { Documents } from "./documents";
 
-export const metadata = { title: "Documents" };
+export const metadata = { title: "Articles" };
 
 export default function DocumentsPage() {
   return <Documents />;
